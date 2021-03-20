dd799c751130e9e42c1a337f78b2d5b4ce662b26
shohamc1 <shhmchk@gmail.com>
2021-03-20 14:59:14 +0800

    WIP: connecting to firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,67 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 const niceware = require("niceware");
 var nodemailer = require("nodemailer");
 
-// pass emailID in body
+admin.initializeApp();
+
+// Helper function to test if tradeID already exists
+const doesTradeExist = (
+  db: FirebaseFirestore.CollectionReference,
+  tradeID: string
+) => {
+  return db
+    .doc(tradeID)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+};
+
+const getHASSMod = (
+  db: FirebaseFirestore.CollectionReference,
+  studentUID: string
+) => {
+  // get all data from user
+  db.doc(studentUID)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        // get modules
+        var modules = doc.data()?.modules;
+
+        // test modules
+        for (let element of modules) {
+          if (/02\.\S+/gm.test(element)) {
+            console.log("Found " + element);
+            return element;
+          }
+        }
+      }
+    });
+};
+
+export const test = functions.https.onRequest(
+  async (req: functions.Request, res: functions.Response) => {
+    const senderID = req.body.ID as string;
+    const db = admin.firestore().collection("users");
+
+    // some async issue here
+    var a = getHASSMod(db, senderID);
+    console.log(a);
+    res.status(400).send("HELLO");
+  }
+);
+
+/*
+ * Request Parameters
+ * @param {string} senderID - UID of requester
+ * @param {string} receiverEID - email ID of requestee
+ */
 export const sendRequest = functions.https.onRequest(
   async (req: functions.Request, res: functions.Response) => {
     // enable CORS
@@ -16,7 +75,6 @@ export const sendRequest = functions.https.onRequest(
       res.status(204).send("");
     }
 
-    //const emailID = req.body.emailID as string;
     var transporter = nodemailer.createTransport({
       host: "smtp.office365.com",
       port: 587,
@@ -31,12 +89,59 @@ export const sendRequest = functions.https.onRequest(
       },
     });
 
-    // generate the trade ID (FORMAT: word-word-word-word)
-    const phrase = niceware.generatePassphrase(8).join("-");
+    // initialise variables
+    var exists = true as boolean;
+    var phrase: string = "";
+    var receiverID: string = "";
+    const db = admin.firestore();
+    const tradeDB = db.collection("trades");
+    const usersDB = db.collection("users");
+
+    // get variables from request body
+    const senderID = req.body.senderID as string;
+    const receiverEID = req.body.senderID as string;
+
+    // check validity of IDs
+    if (
+      !/100\d{4}/gm.test(receiverEID) || //update this test to use email format
+      receiverEID == "" ||
+      receiverEID == null ||
+      senderID == "" ||
+      senderID == null
+    ) {
+      return res.status(400).send("No ID sent or badly formatted").end();
+    }
+
+    // get receiver UID
+    const rec = await usersDB.where("email", "==", receiverEID).get();
+    receiverID = rec.docs[0].data().uid;
+
+    // get current modules
+    const recMod = getHASSMod(usersDB, receiverID);
+    const sendMod = getHASSMod(usersDB, senderID);
+
+    while (exists) {
+      // generate the trade ID (FORMAT: word-word-word-word)
+      phrase = niceware.generatePassphrase(8).join("-");
+
+      // check if trade exists in collection
+      if (!(await doesTradeExist(tradeDB, phrase))) {
+        // add trade to firestore
+        db.doc(phrase).set({
+          from: senderID,
+          fromMod: sendMod,
+          to: receiverID,
+          toMod: recMod,
+        });
+
+        // change value of exists, break from loop
+        exists = false;
+      }
+    }
 
     let mailOptions = {
       from: "sutd-ourportal@outlook.com",
-      to: "shhmchk@gmail.com",
+      to: `${receiverEID}@mymail.sutd.edu.sg`,
       subject: "You've recieved a trade request!",
       text: `Hello there,\nYou've recieved a trade request from someone! Click the link below to check the deets and accept/decline the request.\n\n${
         "https://ourportal.shohamc1.com/trade/" + phrase
@@ -46,9 +151,10 @@ export const sendRequest = functions.https.onRequest(
     transporter.sendMail(mailOptions, (err: any, data: any) => {
       if (err) {
         console.log(err);
+      } else {
+        functions.logger.error(`Initialised trade ${phrase}`);
+        res.sendStatus(200);
       }
-      console.log("Email sent!!!");
-      res.send(200);
     });
   }
 );
