45206b4fc0f3bd544509b7b65a9362ac6533cea1
shohamc1 <shhmchk@gmail.com>
2021-04-01 15:38:18 +0800

    add cors

--- functions/src/exportCSV/index.ts
+++ functions/src/exportCSV/index.ts
@@ -1,16 +1,19 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+var cors = require("cors")({ origin: true });
 
 export const exportCSV = functions.https.onRequest(
   async (req: functions.Request, res: functions.Response) => {
-    const db = admin.firestore();
-    let result = await db.collection("users").get();
-    let data: any = [];
+    cors(req, res, async () => {
+      const db = admin.firestore();
+      let result = await db.collection("users").get();
+      let data: FirebaseFirestore.DocumentData[] = [];
 
-    result.forEach((doc) => {
-      data.push(doc.data());
+      result.forEach((doc) => {
+        data.push(doc.data());
+      });
+
+      res.send(data);
     });
-
-    res.send(data);
   }
 );
